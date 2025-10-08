904bad0d6fcc7007a5bbb942cc6aea1332b91f87
Asphirron <cbuenconsejobusacc@gmail.com>
2025-10-08 22:20:11 +0800

    v2

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if (isset($_SESSION['email'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>TAARAAS - Bicolandia’s Voice for the Voiceless</title>
+  <title>TAARA - Bicolandia’s Voice for the Voiceless</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
   <link rel="stylesheet" href="CSS/essentials.css">
   <script src="https://cdn.tailwindcss.com"></script>
