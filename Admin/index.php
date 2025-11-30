afd1cacfecc89cc1328dd34e53b2b659bce3e67a
Asphirron <cbuenconsejobusacc@gmail.com>
2025-12-01 05:53:51 +0800

    v23.1

--- Admin/index.php
+++ Admin/index.php
@@ -24,11 +24,11 @@ include "../Admin/admin_ui.php"; //Displays Navigation
 <?= displayNav('dashboard'); ?>
 
 <!-- Main Content --->
- <div class='flex'>
+ <div class='flex-c content'>
     <div class="flex-1">
       
       <!-- Dashboard Section ---->
-      <main class="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6" style="margin-left: 15vw">
+      <main class="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         <div class="bg-white shadow rounded-xl p-6">
           <h3 class="font-bold">Animal Profiles</h3>
           <p class="text-gray-600">Manage rescued and adoptable animals.</p>
@@ -56,28 +56,28 @@ include "../Admin/admin_ui.php"; //Displays Navigation
           <div class="bg-white p-6 rounded-xl shadow hover:shadow-lg cursor-pointer">
             <h3 class="font-bold text-lg mb-3">User Management</h3>
             <p class="text-sm text-gray-600">Add, update, or remove volunteers & adopters.</p>
-            <button class="mt-3 px-4 py-2 bg-blue-500 text-white rounded">Manage</button>
+            <a href='animals-records.php'><button class="mt-3 px-4 py-2 bg-green-500 text-white rounded">Manage</button></a>
           </div>
 
           <!-- Manage Animals -->
           <div class="bg-white p-6 rounded-xl shadow hover:shadow-lg cursor-pointer">
             <h3 class="font-bold text-lg mb-3">Animal Management</h3>
             <p class="text-sm text-gray-600">Update profiles, track adoptions, mark rescued.</p>
-            <button class="mt-3 px-4 py-2 bg-green-500 text-white rounded">Manage</button>
+            <a href='animals-records.php'><button class="mt-3 px-4 py-2 bg-green-500 text-white rounded">Manage</button></a>
           </div>
 
           <!-- Manage Donations -->
           <div class="bg-white p-6 rounded-xl shadow hover:shadow-lg cursor-pointer">
             <h3 class="font-bold text-lg mb-3">Donation Management</h3>
             <p class="text-sm text-gray-600">Approve donations and view history.</p>
-            <button class="mt-3 px-4 py-2 bg-purple-500 text-white rounded">Manage</button>
+            <a href='donations-topdonors.php'><button class="mt-3 px-4 py-2 bg-green-500 text-white rounded">Manage</button></a>
           </div>
 
           <!-- Manage Events -->
           <div class="bg-white p-6 rounded-xl shadow hover:shadow-lg cursor-pointer">
             <h3 class="font-bold text-lg mb-3">Event Management</h3>
             <p class="text-sm text-gray-600">Create, edit, or cancel adoption drives.</p>
-            <button class="mt-3 px-4 py-2 bg-orange-500 text-white rounded">Manage</button>
+            <a href='events-upcoming.php'><button class="mt-3 px-4 py-2 bg-green-500 text-white rounded">Manage</button></a>
           </div>
 
           <!-- Reports -->
