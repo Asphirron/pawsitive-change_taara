ff6e7d2556946de9f3c1144e6e01be9d17cdc4d5
Asphirron <cbuenconsejobusacc@gmail.com>
2025-11-30 21:49:14 +0800

    v20.6

--- includes/pdf_template.php
+++ includes/pdf_template.php
@@ -2,6 +2,11 @@
 // /includes/pdf_template.php
 // Corporate PDF generator for TAARA system
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 function pdfImage($filename) {
     if (!$filename) return '';
 
