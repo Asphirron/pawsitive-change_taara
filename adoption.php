68145fbe98ce7eb8ed0eab353648975402be43a8
Asphirron <cbuenconsejobusacc@gmail.com>
2025-12-09 14:22:02 +0800

    v27.5

--- adoption.php
+++ adoption.php
@@ -34,6 +34,7 @@ if (isset($_SESSION['email'])) {
     <link rel="stylesheet" href="CSS/donation.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="CSS/essentials.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Home</title>
    <style>
   body {
